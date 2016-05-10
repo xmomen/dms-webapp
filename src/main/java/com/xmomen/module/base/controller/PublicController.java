c12ab41c6b2fc9f128706d3358d51f1d39ec1f66
Administrator <Administrator@NOMBEVGMXPDQFZN>
2016-05-10 13:11:28 +0800

    权限下拉过滤

--- src/main/java/com/xmomen/module/base/controller/PublicController.java
+++ src/main/java/com/xmomen/module/base/controller/PublicController.java
@@ -1,13 +1,17 @@
 package com.xmomen.module.base.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.shiro.SecurityUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xmomen.framework.mybatis.dao.MybatisDao;
+import com.xmomen.module.base.constant.AppConstants;
 import com.xmomen.module.base.entity.CdCompany;
 import com.xmomen.module.base.mapper.PublicMapper;
 import com.xmomen.module.user.entity.SysUsers;
@@ -31,7 +35,12 @@ public class PublicController {
 	//查询客服经理
 	@RequestMapping(value = "/customerManagerList", method = RequestMethod.GET)
 	public List<SysUsers> getCustomerManager(){
-		List<SysUsers> customerManagerList = publicMapper.getCustomerManager();
+		 Map map = new HashMap<String,Object>();
+		 if(SecurityUtils.getSubject().hasRole(AppConstants.CUSTOMER_MANAGER_PERMISSION_CODE)){
+	            Integer userId = (Integer) SecurityUtils.getSubject().getSession().getAttribute(AppConstants.SESSION_USER_ID_KEY);
+	            map.put("managerId", userId);
+	        }
+		List<SysUsers> customerManagerList = mybatisDao.getSqlSessionTemplate().selectList(PublicMapper.PublicMapperNameSpace+"getManagerList", map);
 		return customerManagerList;
 	}
 }
--- src/main/java/com/xmomen/module/base/mapper/PublicMapper.java
+++ src/main/java/com/xmomen/module/base/mapper/PublicMapper.java
@@ -8,7 +8,6 @@ import org.apache.ibatis.annotations.Select;
 import com.xmomen.module.user.entity.SysUsers;
 
 public interface PublicMapper {
-	@Select(value = "select su.* from sys_users su left join sys_users_roles sur on sur.USER_ID = su.ID left join sys_roles sr on sr.id = sur.ROLE_ID where sr.ROLE ='customer_manager'")
-    @ResultType(SysUsers.class)
-    public List<SysUsers> getCustomerManager();
+	
+    public static final String PublicMapperNameSpace = "com.xmomen.module.base.mapper.PublicMapper.";
 }
