0105db741f0a1e677f32016e7ea7c99d221fe149
taojiaqi <tao_wang_89@163.com>
2017-04-09 17:05:13 +0800

    默认地址

--- src/main/java/com/xmomen/module/wx/module/address/controller/AddressController.java
+++ src/main/java/com/xmomen/module/wx/module/address/controller/AddressController.java
@@ -1,6 +1,8 @@
 package com.xmomen.module.wx.module.address.controller;
 
+import com.xmomen.framework.mybatis.dao.MybatisDao;
 import com.xmomen.framework.web.exceptions.ArgumentValidException;
+import com.xmomen.module.member.entity.MemberAddress;
 import com.xmomen.module.member.service.MemberAddressService;
 import com.xmomen.module.order.entity.TbOrder;
 import com.xmomen.module.order.model.WxCreateOrder;
@@ -30,7 +32,12 @@ public class AddressController {
     @Autowired
     OrderService orderService;
 
+    @Autowired
+    MybatisDao mybatisDao;
+
     /**
+     * 默认收货地址
+     *
      * @param addressId 收货地址
      * @return
      */
@@ -40,5 +47,26 @@ public class AddressController {
             @RequestParam(value = "addressId") String addressId) {
         memberAddressService.defaultAddress(addressId);
         return Boolean.TRUE;
+    }
+
+
+    /**
+     * 获取默认收货地址
+     *
+     * @param memberId 客户ID
+     * @return
+     */
+    @RequestMapping(value = "/wx/getDefaultAddress", method = RequestMethod.GET)
+    @ResponseBody
+    public MemberAddress getDefaultAddress(
+            @RequestParam(value = "memberId") Integer memberId) {
+        MemberAddress memberAddress = new MemberAddress();
+        memberAddress.setCdMemberId(memberId);
+        memberAddress.setIsDefault(true);
+        List<MemberAddress> memberAddresses = this.mybatisDao.selectByModel(memberAddress);
+        if (memberAddresses.size() > 0) {
+            return memberAddresses.get(0);
+        }
+        return null;
     }
 }
