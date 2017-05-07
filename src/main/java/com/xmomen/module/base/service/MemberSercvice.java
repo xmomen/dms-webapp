8c7d15799e9068bfd1845b6cb21aafa9dacedf59
taojiaqi <tao_wang_89@163.com>
2017-05-07 14:00:49 +0800

    查询用户信息

--- src/main/java/com/xmomen/module/base/service/MemberSercvice.java
+++ src/main/java/com/xmomen/module/base/service/MemberSercvice.java
@@ -2,7 +2,9 @@ package com.xmomen.module.base.service;
 
 import com.xmomen.module.base.entity.CdMember;
 import com.xmomen.module.base.model.CreateMember;
+import com.xmomen.module.base.model.MemberModel;
 import com.xmomen.module.base.model.UpdateMember;
+import com.xmomen.module.member.model.MemberAddressModel;
 
 public interface MemberSercvice {
     public void createMember(CreateMember createMember);
@@ -14,4 +16,12 @@ public interface MemberSercvice {
     public CdMember bindMember(String mobile, String openId);
 
     public void updateMobile(Integer id, String mobile);
+
+    /**
+     * 根据主键查询单个对象
+     *
+     * @param id 主键
+     * @return MemberModel 客户对象
+     */
+    public CdMember getOneMemberModel(String id);
 }
